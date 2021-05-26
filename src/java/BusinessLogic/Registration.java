88954c70cb1e48286e7cdf46d393272c0af10477
saquib77 <chand567khan@gmail.com>
2021-05-26 19:13:26 +0530

    view file refactored

--- src/java/BusinessLogic/Registration.java
+++ src/java/BusinessLogic/Registration.java
@@ -9,30 +9,44 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpServletRequest;
 
 public class Registration implements Business {
+    @Override
     public String businessLogic(HttpServletRequest request){
         String fname=request.getParameter("firstname");
         String lname=request.getParameter("lastname");
         String email=request.getParameter("email");
         String phnum=request.getParameter("phnum");
-        String pwd=request.getParameter("password");
-        String city_id=request.getParameter("city_id");
-        String state=request.getParameter("state");
-        String city_name=request.getParameter("");
+        String pwd=request.getParameter("pass");
+        String city_name=request.getParameter("city_name");
+        String state_name=request.getParameter("state_name");
+        String country_name=request.getParameter("country_name");
         String gender=request.getParameter("gender");
         String que=request.getParameter("que");
         String ans=request.getParameter("ans");
         try{
             JavaPOJO JP = new JavaPOJO();
             Statement uidst = JP.createConnection();
-            String fetchedId = "select MAX(uid) from user_table";
+            String fetchedId = "select MAX(uid) from user_master";
             ResultSet rsuid = JP.selectData(uidst, fetchedId);
             int uid=0;
             if(rsuid.next()){
                 uid = rsuid.getInt(1);
             }
             uid = uid+1;
-            String qry = "insert into user_master(uid,firstname,lastname,phone_num,city_id,email,gender)values("+uid+","+fname+","+lname+","+phnum+","+city_id+","+state+")";
-        }catch(Exception e){
+            Statement stmt = JP.createConnection();
+            String fetchedId1 = "select MAX(country_code) from user_master";
+            ResultSet rsuid1 = JP.selectData(stmt, fetchedId1);
+            int code=0;
+            if(rsuid1.next()){
+                uid = rsuid1.getInt(1);
+            }
+            code = code+1;
+            
+            Statement fstmt = JP.createConnection();
+            String qry1 = "insert into user_master(uid,firstname,lastname,phone_num,city_id,email,gender,que,ans)values("+uid+","+fname+","+lname+","+phnum+","+code+","+email+","+gender+","+que+","+ans+")";
+            if(JP.insertData(fstmt, qry1)){
+                
+            }
+        }catch(ClassNotFoundException | SQLException e){
             return e+"Exception";
         }
         return null;
